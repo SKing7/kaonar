a687dd755d8b43437e6daf8877f06a4ed1bc0baf
刘哲 <sking.pt@gmail.com>
2015-05-15 16:52:50 +0800

    注册组件及样式

--- public/page/user/signup.js
+++ public/page/user/signup.js
@@ -1,8 +1,9 @@
 
 var React = require('react');
+var $ = require('jquery');
 var SignupForm = require('component/signup/index');
 
 React.render(
   <SignupForm/>,
-  document.body
+  document.querySelector('.J-main-container')
 );
