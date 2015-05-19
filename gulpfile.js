b33e834f0ae9e2f7050189cc59c244b19baca95c
刘哲 <sking.pt@gmail.com>
2015-05-19 16:21:19 +0800

    add gulp watch and nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
 var gulp = require('gulp');
+var watch = require('gulp-watch');
 var sourcemaps = require('gulp-sourcemaps');
 var react = require('gulp-react');
+var nodemon = require('gulp-nodemon');
  
 var publicSrc = './public/build';
+var jsxSrcPath = 'public/**/*.jsx';
 
-gulp.task('default', function () {
-    return gulp.src('public/**/*.jsx')
+gulp.task('build', function () {
+    gulp.src(jsxSrcPath)
+        .pipe(watch(jsxSrcPath))
+        .pipe(sourcemaps.init())
         .pipe(sourcemaps.init())
         .pipe(react())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(publicSrc));
 });
+gulp.task('startServer', function () {
+  nodemon({
+    script: 'app.js',
+    watch: ['config/**/**'],
+    ext: 'js',
+    env: { 'NODE_ENV': 'development' }
+  })
+})
+gulp.task('dev', ['build', 'startServer'], function () {});
+gulp.task('default', ['build', 'startServer'], function () {});
